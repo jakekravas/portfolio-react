f010394c7e050d114c4214ffbdb05beb18d1b793
Jake Kravas <jakekravas7@gmail.com>
2020-04-04 22:58:19 -0700

    Updates

---
+++ precache-manifest.2b68ba696c6654afb440409f416693fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4171cd240c6af3f68471a05502be16",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "72ae328992d134798864",
+    "url": "/portfolio-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af56381206550c323290",
+    "url": "/portfolio-react/static/js/2.3d0c22cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.3d0c22cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ae328992d134798864",
+    "url": "/portfolio-react/static/js/main.d7bfa0a3.chunk.js"
+  },
+  {
+    "revision": "4c3bcdd375f26df9b5b7",
+    "url": "/portfolio-react/static/js/runtime-main.70e43ac6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
